7303d598e582504d4a64a1426fad78eb1d03c174
Jose Carlos Delgado Ramos <jcdelgadoramos@outlook.com>
2021-08-26 00:08:18 -0500

    #12 page size

--- lyrics_api/swift-lyrics-react/src/App.tsx
+++ lyrics_api/swift-lyrics-react/src/App.tsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Table} from "reactstrap";
+import { Table } from "reactstrap";
 import LyricEntry from "./components/LyricEntry";
 import api from './utils/api';
-import {Lyric} from './types';
+import { Lyric } from './types';
 
 
 function App() {
@@ -12,14 +12,15 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [result, setResult] = useState<Lyric[]>([]);
+  const [querySize, setQuerySize] = useState(25);
 
 
-  async function getLyrics () {
+  async function getLyrics() {
     try {
-      const {data} = await api.get('/lyric/');
+      const { data } = await api.get(`/lyric/?size=${querySize}`);
       if (data === undefined) {
         setError(true);
-        // throw(new Error('Invalid response'));
+        throw(new Error('Invalid response'));
       }
       setResult(data.results);
     } finally {
@@ -27,9 +28,13 @@ function App() {
     }
   };
 
+  const changeQuerySize = (event:any) => {
+    setQuerySize(event.target.value);
+  };
+
   useEffect(() => {
     getLyrics();
-  }, []);
+  }, [querySize]);
 
   if (loading) {
     return (
@@ -61,8 +66,14 @@ function App() {
           <p>
             Find your favorite!
           </p>
+          <label htmlFor="size">Select number of results:</label>
+          <select name="size" value={querySize} onChange={changeQuerySize}>
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+          </select>
           <Table dark>
-           <thead>
+            <thead>
               <tr>
                 <th></th>
                 <th>Lyrics</th>
@@ -73,13 +84,13 @@ function App() {
                 <th></th>
               </tr>
             </thead>
-              <tbody>
-                {
-                  result.map((lyric) => (
-                    <LyricEntry key={lyric.id} lyric={lyric}></LyricEntry>
-                  ))
-                }
-              </tbody>
+            <tbody>
+              {
+                result.map((lyric) => (
+                  <LyricEntry key={lyric.id} lyric={lyric}></LyricEntry>
+                ))
+              }
+            </tbody>
           </Table>
         </header>
       </div>
